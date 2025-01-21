b209b792640e74cc834c8b8fc3599b955ad8b0b3
codemedic213515 <13720882+codemedic213515@users.noreply.github.com>
2025-01-21 20:10:30 +0900

    update production mode

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
-// server.js
-const express = require('express');
-const path = require('path');
+// Import required modules using ES module syntax
+
+import express from 'express';
+
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+// Create an instance of the Express app
+
 const app = express();
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 5173;
+
+// Get the directory name of the current module
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 // Serve static files from the 'dist' directory
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Catch-all route for single-page applications (SPA)
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+
+// Start the server
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
