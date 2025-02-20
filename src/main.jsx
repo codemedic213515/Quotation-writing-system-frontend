64a426f9018c40816bd588affdad9d9fbd7ad3d6
codemedic213515 <13720882+codemedic213515@users.noreply.github.com>
2025-02-20 12:10:55 +0100

    update font style

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import { AuthProvider } from '@/widgets/layout/auth-layout';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@material-tailwind/react';
+import theme from './theme';  
 import { MaterialTailwindControllerProvider } from '@/context';
 import '../public/css/tailwind.css';
 
@@ -11,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <ThemeProvider>
+        <ThemeProvider theme={theme}>
           <MaterialTailwindControllerProvider>
             <App />
           </MaterialTailwindControllerProvider>
--- src/pages/main/calculation.jsx
+++ src/pages/main/calculation.jsx
@@ -199,7 +199,6 @@ export function Calculation() {
           <div className="tab-content">
             {activeTab === 'select' && (
               <SelectQuotation
-              
                 setActiveTab={setActiveTab}
                 setNumber={setNumber}
               />
